f093ffd7fb54b17a0106a0149bf118bcb55483cd
a68660997 <154248551+a68660997@users.noreply.github.com>
2024-10-21 16:00:01 +0500

    Add files via upload

---
+++ Lab01.java
@@ -0,0 +1,10 @@
+package SCD;
+
+public class Lab01 {
+	public static void main(String[] args) {
+		
+		double rad = 60.04;
+		double pi = 3.145;
+		System.out.println("The area of circle is :"+pi*(rad*rad));
+	}
+}
